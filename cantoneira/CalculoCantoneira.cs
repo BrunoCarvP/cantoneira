2b18d93a6e0b7886aded183cec1006070fd9a26a
bruno <bruno@DESKTOP-M4V9171>
2023-08-23 07:05:25 -0300

    Adicionar arquivos de projeto.

---
+++ cantoneira/CalculoCantoneira.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cantoneira
+{
+    internal class CalculoCantoneira
+    {
+        public int Cantoneira(int n)
+        {
+            for (int x = 0;x <=n;x++)
+            {
+                Console.WriteLine(x);
+                if (x == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    for(int y = 1; y<=x;y++)
+                    {
+                        if(x>=y)
+                        {
+                            return y;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ cantoneira/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using cantoneira;
+
+Console.Write("Entre com um valor inteiro: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int resultado;
+CalculoCantoneira calculo = new CalculoCantoneira();
+calculo.Cantoneira(num);
+
+Console.WriteLine(calculo);
